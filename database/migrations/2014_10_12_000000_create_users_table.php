071836db374c9c3922cb46d85d0de1b4163b4193
Fadli1009 <argyfa3@gmail.com>
2023-10-29 11:12:50 -0700

    init awal

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->text('name', 255);
+            $table->text('email', 50)->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password', 50);
+            $table->text('role', 20);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
